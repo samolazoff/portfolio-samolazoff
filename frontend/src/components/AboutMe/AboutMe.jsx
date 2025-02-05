9a0eb3018a9ae4faac1b69c28018e92fb4730872
Samolazoff <samolazov.roman.alex@gmail.com>
2025-02-05 21:59:36 +0300

    Create block about you without content

--- frontend/src/components/AboutMe/AboutMe.jsx
+++ frontend/src/components/AboutMe/AboutMe.jsx
@@ -8,18 +8,23 @@ const AboutMe = () => {
             <div className="block-user">
                 <BigUserImg/>
                 <div className="block-user-btn-wrap">
-                    <button>1</button>
-                    <button>2</button>
-                    <button>3</button>
+                    <button type='button' className='button_accent'>Go for it</button>
+                    <button type='button'>My project</button>
                 </div>
             </div>
             <div className="about-me-content">
                 <h2 className="title-box">About Me</h2>
                 <p className="about-me-content__txt">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime sint quisquam delectus. Esse, ipsam repellendus nam incidunt consequatur vero consequuntur beatae corporis veritatis possimus architecto amet. Earum obcaecati ipsa fuga?</p>
-                <p className="about-me-content__txt">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime sint quisquam delectus. Esse, ipsam repellendus nam incidunt consequatur vero consequuntur beatae corporis veritatis possimus architecto amet. Earum obcaecati ipsa fuga?</p>
             </div>
             <div className="about-me-content">
                 <p className="about-me-content__txt">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime sint quisquam delectus. Esse, ipsam repellendus nam incidunt consequatur vero consequuntur beatae corporis veritatis possimus architecto amet. Earum obcaecati ipsa fuga?</p>
+            </div>
+            <div className="about-me-content">
+                <h2 className="title-box">The last Post</h2>
+                <p className="about-me-content__txt">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime sint quisquam delectus. Esse, ipsam repellendus nam incidunt consequatur vero consequuntur beatae corporis veritatis possimus architecto amet. Earum obcaecati ipsa fuga?</p>
+            </div>
+            <div className="about-me-content">
+                <h2 className="title-box">Current project</h2>
                 <p className="about-me-content__txt">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime sint quisquam delectus. Esse, ipsam repellendus nam incidunt consequatur vero consequuntur beatae corporis veritatis possimus architecto amet. Earum obcaecati ipsa fuga?</p>
             </div>
             
