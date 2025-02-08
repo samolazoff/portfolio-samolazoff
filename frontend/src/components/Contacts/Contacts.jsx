31a6fdf69d1412b741849fe16ec9d3e3592a515e
Samolazoff <samolazov.roman.alex@gmail.com>
2025-02-08 07:53:45 +0100

    Add style address

--- frontend/src/components/Contacts/Contacts.jsx
+++ frontend/src/components/Contacts/Contacts.jsx
@@ -2,6 +2,11 @@ import './Contacts.scss';
 
 import FormConnect from '../FormConnect/FormConnect';
 
+import mobileImg from '../../assets/svg/mobile-phone-signal-svgrepo-com.svg';
+import mailImg from '../../assets/svg/email-opened-svgrepo-com.svg';
+import skypeImg from '../../assets/svg/skype-color-svgrepo-com.svg';
+import telegImg from '../../assets/svg/telegram-svgrepo-com.svg';
+
 const Contacts = () => {
     return (
         <section className='contacts'>
@@ -10,23 +15,26 @@ const Contacts = () => {
                 <div className="block-addresses">
                     <h3 className='subtitle-block'>Connect with Me</h3>
                     <div className='address'>
+                        <img src={mobileImg} alt="svg" />
                         <span className="address_title">Mobile:</span>
                         <address> +375445576259</address>
                     </div>
                     <div className='address'>
+                        <img src={mailImg} alt="svg" />
                         <span className="address_title">Email:</span>
                         <address>samolazov.roman.alex@gmail.com</address>
                     </div>
                     <div className='address'>
+                        <img src={skypeImg} alt="svg" />
                         <span className="address_title">Skype:</span>
                         <address>samolazov.roman.alex</address>
                     </div>
                     <div className='address'>
+                        <img src={telegImg} alt="svg" />
                         <span className="address_title">Telegrame:</span>
                         <address>@samolazoff</address>
                     </div>
                     <div className="block-social-media">
-
                     </div>
                 </div>
                 <FormConnect/> 
