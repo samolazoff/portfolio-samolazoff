ea130f76d2aebc27c440b728b6a7156ec19b25b0
Samolazoff <samolazov.roman.alex@gmail.com>
2025-02-10 12:01:00 +0100

    Implement new view portfolio card

--- frontend/src/components/PortfolioCard/PortfolioCard.jsx
+++ frontend/src/components/PortfolioCard/PortfolioCard.jsx
@@ -4,16 +4,17 @@ import { Link } from 'react-router-dom';
 
 const PortfolioCard = (props) => {
 
-    const {title, link, description, img, note, id} = props.card;
+    const {title, link, description, note, id} = props.card;
 
     return (
         <div className='portfolio-card' id={id}>
-            <img src={img} alt='img' className="portfolio-card__img" />
-            <Link className="portfolio-card-review"  to={link}>
-                <span className="title">{title}</span>
-                <span className="txt">{description}</span>
-                <span className="commit">{`Notes: ${note}`}</span>
-            </Link>
+                <iframe src={link}></iframe>
+                <Link className="portfolio-card-review" to={link}>
+                    <span className="title">{title}</span>
+                    <span className="txt">{description}</span>
+                    <span className="commit">{`Notes: ${note}`}</span>
+                </Link>
+                
         </div>
     );
 };
